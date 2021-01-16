21e8d61aa1c8aea4b72fc0a3642630a8a24b0128
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-16 10:27:18 -0500

    auth stuffs

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,26 +1,50 @@
 import { AuthChecker } from 'type-graphql'
-
 import firebaseAdmin from 'firebase-admin'
-import 'firebase/firestore'
-import 'firebase/auth'
 
 export const authChecker: AuthChecker<any> = async (e: any, a: any) => {
 	let authed = false
 
 	const {
 		context: { headers },
-		// info: { fieldName, fieldNodes }
+		info: { fieldName, fieldNodes },
 	} = e
 
-	if (a.includes('admin') && headers.admin) {
-		authed = process.env.admin === headers.admin
-	} else if (a.includes('authed') && headers.token) {
-		authed = await firebaseAdmin
-			.auth()
-			.verifyIdToken(headers.token)
-			.then(() => true)
-			.catch(() => false)
+	console.log(fieldNodes[0].arguments)
+	console.log(fieldName)
+
+	console.log(e.args)
+	authed = process.env.admin === headers?.admin
+	if (!authed) {
+		// eslint-disable-next-line prefer-const
+		let uidForMatch = 'VabmFGaffURVNB5Cq56GWva4vcD2'
+
+		if (fieldName === 'createUser') {
+			uidForMatch =
+				fieldNodes[0].arguments[0].value.fields.filter(
+					(x: any) => x.name.value === 'uid'
+				)[0].value.value ||
+				e.args.data[
+					fieldNodes[0].arguments[0].value.fields.filter(
+						(x: any) => x.name.value === 'uid'
+					)[0].value.name.value
+				]
+		} else if (fieldName === 'user') {
+			console.log(
+				fieldNodes[0].arguments[0].value.value ||
+					e.args[fieldNodes[0].arguments[0].value.name.value]
+			)
+		}
+
+		// if (a.includes('admin') && headers.admin) {
+		// 	authed = process.env.admin === headers.admin
+		// } else if (a.includes('authed') && headers.token) {
+		const x = await firebaseAdmin.auth().verifyIdToken(headers.token)
+
+		console.log(x)
+
+		authed = x.uid === uidForMatch
 	}
+	// }
 
 	return authed // or false if access is denied
 }
