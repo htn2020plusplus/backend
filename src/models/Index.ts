0b4f6bcd9c0810c032e5bed36a4d8761fa6a849f
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-16 04:42:40 -0500

    index model

--- src/models/Index.ts
+++ src/models/Index.ts
@@ -7,7 +7,7 @@ import {
 } from 'typeorm'
 import { ObjectType, Field, ID } from 'type-graphql'
 import NamedEntity from './NamedEntity'
-import LegislationEvent from './Events'
+import LegislationEvent from './Event'
 
 @ObjectType()
 @Entity()
@@ -18,21 +18,17 @@ export default class Index extends BaseEntity {
 
 	@Field()
 	@Column()
-	lineNumber: number
-
-	@Field()
-	@Column()
 	startPosition: number
 
 	@Field()
 	@Column()
 	endPosition: number
 
-	@ManyToOne(() => NamedEntity, (ne) => ne.indices)
-	@Field(() => NamedEntity)
-	entity: NamedEntity
+	@ManyToOne(() => NamedEntity, (ne) => ne.indices, { nullable: true })
+	@Field(() => NamedEntity, { nullable: true })
+	entity?: NamedEntity
 
-	@ManyToOne(() => LegislationEvent, (le) => le.indices)
-	@Field(() => LegislationEvent)
-	legislationEvent: LegislationEvent
+	@ManyToOne(() => LegislationEvent, (le) => le.indices, { nullable: true })
+	@Field(() => LegislationEvent, { nullable: true })
+	legislationEvent?: LegislationEvent
 }
