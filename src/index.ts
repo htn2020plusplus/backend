5884fb07de7c0848515e686b0c5a5f64fff61965
Rishi Kothari <itsrishikothari@gmail.com>
2021-01-17 02:20:12 -0500

    MOVE TO POSTGRES AAAAAAAAA

--- src/index.ts
+++ src/index.ts
@@ -33,16 +33,31 @@ firebaseAdmin.initializeApp({
 })
 
 async function main() {
+	// await createConnection({
+	// 	type: 'cockroachdb',
+	// 	synchronize: true,
+	// 	url: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}.${process.env.DB_DB}`,
+	// 	host: `${process.env.DB_HOST}`,
+	// 	ssl: {
+	// 		rejectUnauthorized: false,
+	// 		ca: fs.readFileSync(path.resolve(__dirname, 'certs/be_cert.crt')),
+	// 	},
+	// 	entities: [NamedEntity, Category, Policy, Index, LegislationEvent, User],
+	// })
+
 	await createConnection({
-		type: 'cockroachdb',
+		type: 'postgres',
+		host: 'ec2-3-220-98-137.compute-1.amazonaws.com',
+		database: 'dev989papr3p0t',
+		username: 'ijbzzutzyofbdt',
+		port: 5432,
+		password:
+			'82e8f3126462cec6f5f939aeb5145dfa911a500776b742a0bdbc0f1571cdc0df',
+		entities: [NamedEntity, Category, Policy, Index, LegislationEvent, User],
 		synchronize: true,
-		url: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}.${process.env.DB_DB}`,
-		host: `${process.env.DB_HOST}`,
 		ssl: {
 			rejectUnauthorized: false,
-			ca: fs.readFileSync(path.resolve(__dirname, 'certs/be_cert.crt')),
 		},
-		entities: [NamedEntity, Category, Policy, Index, LegislationEvent, User],
 	})
 
 	console.log(
